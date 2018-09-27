f00531c79adf3d3f790bfbea375a5cc3875fad3e
Anwesh43 <anweshthecool0@gmail.com>
2018-09-27 17:00:11 +0530

    adding drawing fixes

--- bisquaredotview/src/main/java/com/anwesh/uiprojects/bisquaredotview/BiSquareDotView.kt
+++ bisquaredotview/src/main/java/com/anwesh/uiprojects/bisquaredotview/BiSquareDotView.kt
@@ -19,35 +19,35 @@ val nodes : Int = 5
 fun Canvas.drawBSDNode(i : Int, scale : Float, paint : Paint) {
     val w : Float = width.toFloat()
     val h : Float = height.toFloat()
-    val gap : Float = h / nodes
-    val size : Float = gap / 3
-    val r : Float = size/5
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / 3.5f
+    val r : Float = size/3
     val sc1 : Float = Math.min(0.5f, scale) * 2
     val sc2 : Float = Math.min(0.5f, Math.max(scale - 0.5f, 0f)) * 2
-    val sf : Float = 1f - 2 * i
+    val sf : Float = 1f - 2 * (i % 2)
     paint.strokeWidth = Math.min(w, h) / 60
     paint.strokeCap = Paint.Cap.ROUND
     paint.color = Color.parseColor("#4A148C")
     save()
-    translate(w/2 + sf * sc2 * (w/2 - size), h/2)
+    translate(w/2 + sf * sc2 * (w/2 - size -r), gap + i * gap)
     rotate(90f * sf * sc2)
     for (j in 0..1) {
         val sc : Float = Math.min(0.5f, Math.max(0f, sc1 - 0.5f * j)) * 2
         val sj : Float = 1f - 2 * j
         save()
         scale(sj, sj)
-        for (p in 0..2) {
+        for (p in 0..1) {
             save()
-            translate(size / 2, size/2 * (1 - 2 * p))
+            translate(size , size * (1 - 2 * p))
             paint.style = Paint.Style.STROKE
             drawCircle(0f, 0f, r, paint)
             paint.style = Paint.Style.FILL
             drawArc(RectF(-r, -r, r, r), 0f, 360f * sc, true, paint)
             restore()
-            val a : Float = size/2 * sc * p
-            val b : Float = size/2 * sc * (1 - p)
-            val a1 : Float = size/2 * (1 - p)
-            val b1 : Float = size/2 * p
+            val a : Float = size * sc * p
+            val b : Float = size * sc * (1 - p)
+            val a1 : Float = size * (1 - p)
+            val b1 : Float = size * p
             drawLine(-a + a1, -b + b1, a + a1, b + b1, paint)
         }
         restore()
