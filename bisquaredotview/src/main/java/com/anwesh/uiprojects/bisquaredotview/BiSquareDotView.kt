137846034a397b95a176d00e5c14238aba9234db
Anwesh43 <anweshthecool0@gmail.com>
2018-09-27 14:08:37 +0530

    adding bisquaredotview module

--- bisquaredotview/src/main/java/com/anwesh/uiprojects/bisquaredotview/BiSquareDotView.kt
+++ bisquaredotview/src/main/java/com/anwesh/uiprojects/bisquaredotview/BiSquareDotView.kt
@@ -4,3 +4,12 @@ package com.anwesh.uiprojects.bisquaredotview
  * Created by anweshmishra on 27/09/18.
  */
 
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
