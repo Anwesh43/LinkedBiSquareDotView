7d17744054209479c769a411d14fdabdf4a9704b
Anwesh43 <anweshthecool0@gmail.com>
2018-09-27 16:56:09 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbisquardotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbisquardotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbisquardotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bisquaredotview.BiSquareDotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiSquareDotView.create(this)
     }
 }
