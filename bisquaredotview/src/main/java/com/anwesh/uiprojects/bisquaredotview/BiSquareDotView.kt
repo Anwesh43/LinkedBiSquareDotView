1591fac6f4df21d3db1fb01572841fbdb9727249
Anwesh43 <anweshthecool0@gmail.com>
2018-09-27 15:25:08 +0530

    adding BSDNode class

--- bisquaredotview/src/main/java/com/anwesh/uiprojects/bisquaredotview/BiSquareDotView.kt
+++ bisquaredotview/src/main/java/com/anwesh/uiprojects/bisquaredotview/BiSquareDotView.kt
@@ -118,4 +118,49 @@ class BiSquareDotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSDNode(var i : Int, val state : State = State()) {
+
+        private var next : BSDNode? = null
+
+        private var prev : BSDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BSDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSDNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSDNode {
+            var curr : BSDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
